dd86345f96187ad5ac0ef828b8ef95197c163b63
samuel-ma <mabilmakur@gmail.comm>
2022-09-16 02:00:37 +0200

    first commit

---
+++ soniq/src/App.js
@@ -0,0 +1,12 @@
+import Main from "./Pages/Main";
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ soniq/src/Pages/Main.js
@@ -0,0 +1,30 @@
+import "../styles/Main.css";
+import Logo from "../images/image2.png";
+
+function Main() {
+  return (
+    <div className="container">
+        <div>
+            <h1>Three Portfolios</h1>
+            <h2>Mind Blowing Experiences <div className="box"></div> </h2>
+            <h3>Choose One :</h3>
+        </div>
+
+        <div className="main">
+                <div className="first-main">
+                    {/* <img src={Logo} className="image"/> */}
+                </div>
+
+                <div className="first-main2">
+                </div>
+
+                <div className="first-main3">
+                </div>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Main
